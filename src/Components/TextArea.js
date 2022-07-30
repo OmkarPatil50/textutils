d823fcdec24f8f6fccb06e4500a16f2a1f7c063b
Omkar Patil <Omkarp5429@gmail.com>
2022-07-30 11:19:54 +0530

    buttons updated

--- src/Components/TextArea.js
+++ src/Components/TextArea.js
@@ -4,7 +4,8 @@ import React, { useState } from 'react';
 
 export default function TextArea() {
 
-    const [text, setText] = useState("")
+    const [text, setText] = useState("");
+    const[result,setResult] = useState('')
 
     const upperCaseHandler = () => {
         const upperCaseText = text.toUpperCase();
@@ -21,7 +22,36 @@ export default function TextArea() {
         setText(event.target.value)
     }
 
+const wordCountHandler=()=>{
+    const wordsNumber = text.split(" ").length;
+    if(text){
+        setResult("Number of words: " + wordsNumber)
 
+    }else{
+        setResult("Number of words: 0")
+    }
+}
+
+const characterCountHandler=()=>{
+    const charactersNUmber= text.length;
+    setResult("Number of Characters: "+ charactersNUmber)
+}
+
+const readingTimeHandler=()=>{
+    const readingTime = (text.split(" ").length * 0.008);
+    if(text){
+        setResult("Reading Time Required is approximately " + readingTime + "min.")
+      
+    }else{
+        setResult("Please enter text to analyse...!" )
+    }
+}
+
+
+const clearTextHandler =()=>{
+    setResult("")
+    setText("")
+}
 
 
 
@@ -32,24 +62,35 @@ export default function TextArea() {
             <div className='container my-3'>
                 <div className="form-floating my-3">
                     <div className="mb-3">
-                        <textarea className="form-control" value={text} onChange={onChangeHandler} id="exampleFormControlTextarea1" rows="12" placeholder='Enter Your Text Here'></textarea>
+                        <textarea className="form-control" required value={text} onChange={onChangeHandler} id="exampleFormControlTextarea1" rows="12" placeholder='Enter Your Text Here'></textarea>
                     </div>
-                    <button type="button" className="btn btn-info mx-3" onClick={upperCaseHandler}>Convert to Uppercase</button>
-                    <button type="button" className="btn btn-outline-info mx-3" onClick={lowerCaseHandler}>Convert to Lowercase</button>
 
                 </div>
             </div>
 
-            <div className="container my-3">
-                <h2 className="my-3" >Your Text Summary</h2>
-                <p className="my-2">Your Text has {text.split(" ").length} words and {text.length} characters.</p>
-                <p className="my-2">Read Time: {(text.split(" ").length*0.008)} min</p>
+
+
+            <button type="button" className="btn btn-info mx-3" onClick={upperCaseHandler}>Convert to Uppercase</button>
+            <button type="button" className="btn btn-outline-info mx-3" onClick={lowerCaseHandler}>Convert to Lowercase</button>
+            <button type="button" className="btn btn-secondary mx-3" onClick={wordCountHandler}>Count Words</button>
+            <button type="button" className="btn btn-warning mx-3" onClick={characterCountHandler}>Count Characters</button>
+            <button type="button" className="btn btn-primary mx-3" onClick={readingTimeHandler}>Measure Reading Time</button>
+            <button type="button" className="btn btn-danger mx-3" onClick={clearTextHandler}>Clear Text</button>
+
+
+
+            <div className="container my-5">
+                <h3>{result}</h3>
             </div>
+
+<hr className='my-5 mx-3' />
+
             <div className="container my-3">
-                <h2 className="my-2">Preview:</h2>
+                <h2 className="my-4">Preview:</h2>
                 <p>{text}</p>
-</div>
-            
+            </div>
+    
         </>
     )
 }
+
