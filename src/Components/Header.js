bd6a942edfba080e2fe361c5ec7d21ced8f9d281
Omkar Patil <Omkarp5429@gmail.com>
2022-08-05 12:25:23 +0530

    final commit

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -21,7 +21,9 @@ export default function Header(props) {
           <Link className="nav-link active" aria-current="page" to="/" >Home</Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to="/about">About</Link>
+          <Link className="nav-link" to="/about" onClick={()=>{
+            document.title="TextUtils - About"
+          }}>About</Link>
         </li>
         
    
--- src/Components/TextArea.js
+++ src/Components/TextArea.js
@@ -5,29 +5,23 @@ import React, { useState } from 'react';
 export default function TextArea(props) {
 
     const [text, setText] = useState("");
-    const [result, setResult] = useState('')
 
     const upperCaseHandler = () => {
-        if (text) {
-            const upperCaseText = text.toUpperCase();
-            setText(upperCaseText)
-            props.alertHandler("Converted to Upper-Case Successfully!", "success")
-        } else {
-            props.alertHandler("Please Enter Text in Textbox", "danger")
 
-        }
+        const upperCaseText = text.toUpperCase();
+        setText(upperCaseText)
+        props.alertHandler("Converted to Upper-Case Successfully!", "success")
+
 
     }
 
 
     const lowerCaseHandler = () => {
-        if (text) {
-            const lowerCaseText = text.toLowerCase()
-            setText(lowerCaseText)
-            props.alertHandler("Converted to Lower-Case Successfully!", "success")
-        } else {
-            props.alertHandler("Please Enter Text in Textbox", "danger")
-        }
+
+        const lowerCaseText = text.toLowerCase()
+        setText(lowerCaseText)
+        props.alertHandler("Converted to Lower-Case Successfully!", "success")
+
 
     }
 
@@ -35,88 +29,31 @@ export default function TextArea(props) {
         setText(event.target.value)
     }
 
-    const wordCountHandler = () => {
-        if (text) {
-            const wordsNumber = text.split(" ").filter((element)=>{
-return element.length!==0
-            }).length;
-                setResult("Number of words: " + wordsNumber)
-                props.alertHandler("Number of Words Counted Successfully!", "success")
-        } else {
-            setResult(null)
-            props.alertHandler("Please Enter Text in Textbox", "danger")
-
-        }
+ 
 
 
-    }
-
-    const characterCountHandler = () => {
-
-        if (text) {
-            const charactersNUmber = text.split('').filter((word)=>{
-                return word!==" "
-            }).length;
-            setResult("Number of Characters: " + charactersNUmber)
-            props.alertHandler("Number of Characters Counted Successfully!", "success")
-        } else {
-            props.alertHandler("Please Enter Text in Textbox", "danger")
-
-        }
-
-
-
-    }
-
-    const readingTimeHandler = () => {
-
-        if (text) {
-
-            const readingTime = (text.split(" ").filter((element)=>{
-                return element.length!==0
-                            }).length * 0.008);
    
-                setResult("Reading Time Required is approximately " + readingTime + "min.")
-                props.alertHandler("Reading Time Measured Successfully!", "success")
-
-         
-
-        } else {
-            props.alertHandler("Please Enter Text in Textbox", "danger")
-            setResult(null)
-        }
-
-
-
-
-    }
 
     const copyTextHandler = () => {
 
-        if (text) {
-            var copiedText = document.getElementById("exampleFormControlTextarea1")
-            navigator.clipboard.writeText(copiedText.value)
-            props.alertHandler("Text Copied Successfully!", "success")
-        } else {
-            props.alertHandler("Please Enter Text in Textbox", "danger")
 
-        }
+        var copiedText = document.getElementById("exampleFormControlTextarea1")
+        navigator.clipboard.writeText(copiedText.value)
+        props.alertHandler("Text Copied Successfully!", "success")
+
 
 
 
     }
 
     const cutTextHandler = () => {
-        if (text) {
 
-            let copiedText = document.getElementById("exampleFormControlTextarea1")
-            navigator.clipboard.writeText(copiedText.value)
-            props.alertHandler("Text cut Successful!", "success")
-            setText("")
-        } else {
-            props.alertHandler("Please Enter Text in Textbox", "danger")
 
-        }
+        let copiedText = document.getElementById("exampleFormControlTextarea1")
+        navigator.clipboard.writeText(copiedText.value)
+        props.alertHandler("Text cut Successful!", "success")
+        setText("")
+
 
 
     }
@@ -124,15 +61,12 @@ return element.length!==0
 
     const removeExtraSpaceHandler = () => {
 
-        if (text) {
-            let newText = text.split(/[ ]+/)
-            setText(newText.join(" "))
-            props.alertHandler("Extra Spaces Removed Successfully!", "success")
 
-        } else {
-            props.alertHandler("Please Enter Text in Textbox", "danger")
+        let newText = text.split(/[ ]+/)
+        setText(newText.join(" "))
+        props.alertHandler("Extra Spaces Removed Successfully!", "success")
 
-        }
+
 
 
 
@@ -141,7 +75,7 @@ return element.length!==0
 
 
     const clearTextHandler = () => {
-        setResult("")
+    
         setText("")
         props.alertHandler("Text Cleared Successfully!", "success")
     }
@@ -157,6 +91,13 @@ return element.length!==0
                     <div className="form-floating py-5">
                         <div className="mb-3">
                             <textarea className={`form-control bg-${props.mode === "light" ? "light" : "dark"} text-${props.mode === "dark" ? "light" : "dark"}`} required value={text} onChange={onChangeHandler} id="exampleFormControlTextarea1" rows="12" placeholder='Enter Your Text Here'></textarea>
+                            
+                <button disabled={text.length===0} type="button" className="btn btn-info mx-3 my-3" onClick={upperCaseHandler}>Convert to Uppercase</button>
+                <button  disabled={text.length===0} type="button" className="btn btn-outline-info mx-3 my-3" onClick={lowerCaseHandler}>Convert to Lowercase</button>
+                <button  disabled={text.length===0} type="button" className="btn btn-success mx-3 my-3" onClick={copyTextHandler}>Copy Text</button>
+                <button  disabled={text.length===0} type="button" className="btn btn-light mx-3 my-3" onClick={cutTextHandler}>Cut Text</button>
+                <button  disabled={text.length===0} type="button" className={`btn btn-${props.mode === "light" ? "outline-dark" : "outline-light"}`} onClick={removeExtraSpaceHandler}>Remove Extra Spaces</button>
+                <button  disabled={text.length===0} type="button" className="btn btn-danger mx-3 my-3" onClick={clearTextHandler}>Clear Text</button>
                         </div>
 
                     </div>
@@ -164,25 +105,24 @@ return element.length!==0
 
 
 
-                <button type="button" className="btn btn-info mx-3 my-3" onClick={upperCaseHandler}>Convert to Uppercase</button>
-                <button type="button" className="btn btn-outline-info mx-3 my-3" onClick={lowerCaseHandler}>Convert to Lowercase</button>
-                <button type="button" className="btn btn-secondary mx-3 my-3" onClick={wordCountHandler}>Count Words</button>
-                <button type="button" className="btn btn-warning mx-3 my-3" onClick={characterCountHandler}>Count Characters</button>
-                <button type="button" className="btn btn-primary mx-3 my-3" onClick={readingTimeHandler}>Measure Reading Time</button>
-                <button type="button" className="btn btn-success mx-3 my-3" onClick={copyTextHandler}>Copy Text</button>
-                <button type="button" className="btn btn-light mx-3 my-3" onClick={cutTextHandler}>Cut Text</button>
-                <button type="button" className={`btn btn-${props.mode === "light" ? "outline-dark" : "outline-light"}`} onClick={removeExtraSpaceHandler}>Remove Extra Spaces</button>
-                <button type="button" className="btn btn-danger mx-3 my-3" onClick={clearTextHandler}>Clear Text</button>
 
 
-                <div className="container my-5">
-                    <h3 className={`text-${props.mode === "dark" ? "light" : "dark"} py-4`}>{result}</h3>
+                <div className="container">
+                    <h3 className={`text-${props.mode === "dark" ? "light" : "dark"} py-2`}>Text Summary</h3>
+                    <p>{text.split(" ").filter((element) => {
+            return element.length !== 0
+        }).length} Words and {text.split('').filter((word) => {
+            return word !== " "
+        }).length} Charaters</p>
+        <p>Reading Time: {(text.split(" ").filter((element) => {
+            return element.length !== 0
+        }).length * 0.008)} Minutes </p>
                 </div>
 
-                <hr className='my-5 mx-3' />
+                <hr className='my-4 mx-3' />
 
-                <div className="container my-3">
-                    <h2 className={`text-${props.mode === "dark" ? "light" : "dark"} py-4`}>Preview:</h2>
+                <div className="container my-2">
+                    <h2 className={`text-${props.mode === "dark" ? "light" : "dark"} `}>Preview:</h2>
                     <p className={`text-${props.mode === "dark" ? "light" : "dark"} pb-5 `}>{text.length > 0 ? text : "Please Enter Your Text in Textbox to Get Preview"}</p>
                 </div>
 
